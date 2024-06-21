4762d1b11d0d53f46c48871b4e6f3ca24d995a2e
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-21 22:31:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,12 +32,6 @@ connectToMongoDB(mongoURI)
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use(
   cors({
