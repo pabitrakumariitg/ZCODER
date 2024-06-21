fbcae041ae61499cea22221dadcfb9721b02840b
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-21 22:15:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,7 +31,11 @@ connectToMongoDB(mongoURI)
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors('*'));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Routes
 app.use("/signUp", SignUpRouter);
