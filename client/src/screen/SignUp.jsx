04eb9f65eff13ad856a08b02dc7deb0e26e45487
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-29 20:18:27 +0530

    Update SignUp.jsx

--- client/src/screen/SignUp.jsx
+++ client/src/screen/SignUp.jsx
@@ -33,7 +33,7 @@ const SignUp = () => {
     }
 
     try {
-      const response = await fetch("https://zcoder-api.vercel.app/signup", {
+      const response = await fetch("https://zcoder-api.vercel.app/signUp", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
