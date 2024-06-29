0d040f8a988ca1eb0481966ea5786281eb6171c4
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-29 21:59:23 +0530

    Update Login.js

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {handleLogin} = require("../controller/Login");
+const { handleLogin } = require("../controller/Login");
 
-router.route("/").post(handleLogin);
+router.route("/login").post(handleLogin);
 
 module.exports = router;
