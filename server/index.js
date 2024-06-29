17e664b5754519349c057d8aaa12bd0a7745a3f9
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-29 19:43:21 +0530

    dcdsw

--- server/index.js
+++ server/index.js
@@ -1,51 +1,51 @@
 const express = require("express");
 const cors = require("cors");
-const { connectToMongoDB } = require("./connect");
+// const { connectToMongoDB } = require("./connect");
 
-// Import routers
-const SignUpRouter = require("./routes/SignUp");
-const LoginRouter = require("./routes/Login");
-const UploadedQuestionRouter = require("./routes/UploadQuestion");
-const EditProfileRouter = require("./routes/EditProfile");
-const GetProfileRouter = require("./routes/Profile");
-const GetRecentQuestionRouter = require("./routes/FetchRecentQuestions");
-const GetMyStackRouter = require("./routes/MyStack");
-const UpdateQuestionRouter = require("./routes/UpdateQuestion");
-const GetExploreRouter = require("./routes/Explore");
-const CodeLiveRouter = require("./routes/CodeLive");
+// // Import routers
+// const SignUpRouter = require("./routes/SignUp");
+// const LoginRouter = require("./routes/Login");
+// const UploadedQuestionRouter = require("./routes/UploadQuestion");
+// const EditProfileRouter = require("./routes/EditProfile");
+// const GetProfileRouter = require("./routes/Profile");
+// const GetRecentQuestionRouter = require("./routes/FetchRecentQuestions");
+// const GetMyStackRouter = require("./routes/MyStack");
+// const UpdateQuestionRouter = require("./routes/UpdateQuestion");
+// const GetExploreRouter = require("./routes/Explore");
+// const CodeLiveRouter = require("./routes/CodeLive");
 
 const app = express();
 const PORT = 8000;
 
 // MongoDB connection setup
-const mongoURI = "mongodb+srv://pabitraKumar:Pabitra@cluster0.bqxhvj3.mongodb.net/goFood?retryWrites=true&w=majority&appName=Cluster0";
-connectToMongoDB(mongoURI)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+// const mongoURI = "mongodb+srv://pabitraKumar:Pabitra@cluster0.bqxhvj3.mongodb.net/goFood?retryWrites=true&w=majority&appName=Cluster0";
+// connectToMongoDB(mongoURI)
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection error:", err);
+//   });
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // Middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-// CORS Configuration
-const corsOptions = {
-  origin: 'https://zcoder-api.vercel.app', // Allow your front-end origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
+// // CORS Configuration
+// const corsOptions = {
+//   origin: 'https://zcoder-api.vercel.app', // Allow your front-end origin
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-// Middleware to simulate getting the current username (for demonstration purposes)
-app.use((req, res, next) => {
-  req.currentUsername = "currentUsername"; // Replace with actual logic to get the username
-  next();
-});
+// // Middleware to simulate getting the current username (for demonstration purposes)
+// app.use((req, res, next) => {
+//   req.currentUsername = "currentUsername"; // Replace with actual logic to get the username
+//   next();
+// });
 
 // Routes
 app.use("/",(req,res)=>{
